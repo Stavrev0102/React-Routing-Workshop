42bd82377b26d9837c774aef18297e092842c77c
Ivaylo Stavrev <114228905+Stavrev0102@users.noreply.github.com>
2023-11-14 15:43:06 +0200

    Add Header component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,11 @@
+import Header from "./components/Header/Header"
 
 function App() {
 
   return (
-    <>
-     <h1>Hello world</h1>
-    </>
+    <div id="box">
+     <Header/>
+    </div>
   )
 }
 
