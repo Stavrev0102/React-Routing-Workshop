f638deba1f85c14dffcaec7d1a0033d41a7b8f7d
Ivaylo Stavrev <114228905+Stavrev0102@users.noreply.github.com>
2023-11-21 16:19:49 +0200

    Add keys

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import GameCreate from "./components/Game-Create/GameCreate";
 import GameList from "./components/Game-List/GameList";
 import Header from "./components/Header/Header"
@@ -6,8 +7,14 @@ import { Routes,Route } from 'react-router-dom';
 import Login from "./components/Login/Login";
 import Register from "./components/Register/Register";
 import GameDetails from "./components/Game-Details/GameDetails";
+import { useState } from "react";
 
 function App() {
+  const [auth,setAuth] = useState({});
+
+  const loginSubmitHandler = (values) => {
+    console.log(values);
+  }
 
   return (
     <div id="box">
@@ -18,7 +25,7 @@ function App() {
         <Route path="/" element={ <Home/> }/>
         <Route path="/games" element={<GameList/>}/>
         <Route path="/games/create" element={<GameCreate/>}/>
-        <Route path="/login" element={<Login/>}/>
+        <Route path="/login" element={<Login loginSubmitHandler={loginSubmitHandler}/>}/>
         <Route path="/register" element={<Register/>}/>
         <Route path="/games/:gameId" element={<GameDetails/>}/>
       </Routes>
--- client/src/components/Login/Login.jsx
+++ client/src/components/Login/Login.jsx
@@ -1,19 +1,26 @@
+/* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
 /* eslint-disable react/no-unescaped-entities */
 import {Link} from 'react-router-dom';
-
 import useForm from "../../hooks/useForm";
 
-export default function Login(){
+const loginFormKeys = {
+  Email:'email',
+  Password:'password'
+}
 
-  const { values,onChange,onSubmit} = useForm({
-    email:'',
-    password:''
+export default function Login({
+  loginSubmitHandler
+}){
+
+  const { values,onChange,onSubmit} = useForm(loginSubmitHandler,{
+    [loginFormKeys.Email]:'',
+    [loginFormKeys.Password]:''
   });
 
     return (
       <section id="login-page" className="auth">
-        <form onSubmit={onSubmit }id="login">
+        <form onSubmit={onSubmit } id="login">
 
           <div className="container">
             <div className="brand-logo" />
@@ -22,18 +29,18 @@ export default function Login(){
             <input
               type="email"
               id="email"
-              name="email"
+              name={loginFormKeys.Email}
               placeholder="Sokka@gmail.com"
               onChange={onChange}
-              value={values.email}
+              value={values[loginFormKeys.Email]}
             />
             <label htmlFor="login-pass">Password:</label>
             <input
              type="password" 
              id="login-password"
-             name="password"
+             name={loginFormKeys.Password}
              onChange={onChange}
-             value={values.password}
+             value={values[loginFormKeys.Password]}
 
 
              />
