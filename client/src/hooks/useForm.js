e233bbe98287b065caf074d536660efbe8dfb169
Ivaylo Stavrev <114228905+Stavrev0102@users.noreply.github.com>
2023-11-21 16:03:56 +0200

    Create custom hook useForm

---
+++ client/src/hooks/useForm.js
@@ -0,0 +1,25 @@
+/* eslint-disable no-unused-vars */
+import { useState } from "react";
+
+export default function useForm(submitHandler,initialValues){
+    const [values,setValues] = useState(initialValues)
+
+    const onChange = (e) => {
+            setValues(state => ({
+                ...state,
+                [e.target.name]:e.target.value
+            }))
+    };
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+
+        submitHandler(values)
+    }
+        
+    return {
+        values,
+        onChange,
+        onSubmit
+    }
+}
